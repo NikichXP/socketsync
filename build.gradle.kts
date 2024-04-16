5cb614c5868b3e22f00871bafaa856e5319a8c45
Nikita <nikichx2@gmail.com>
2024-04-16 22:18:38 +0200

    first steps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,20 @@
+val ktorVersion = "2.3.10"
+
 plugins {
     kotlin("jvm") version "1.9.23"
 }
 
 group = "com.nikichxp"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.ktor:ktor-network:$ktorVersion")
+    implementation("io.ktor:ktor-network-tls:$ktorVersion")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,58 @@
 package com.nikichxp
 
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.util.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.nio.ByteBuffer
+import java.util.concurrent.Executors
+import kotlin.coroutines.*
+
 fun main() {
-    println("Hello World!")
+    val executor = Executors.newFixedThreadPool(4).asCoroutineDispatcher()
+
+    runBlocking(executor) {
+        launch {
+            sender()
+        }
+        launch {
+            receiver()
+        }
+    }
+
+}
+
+suspend fun sender() {
+    val selectorManager = SelectorManager(Dispatchers.IO)
+    val serverSocket = aSocket(selectorManager).tcp().bind("127.0.0.1", 9002)
+    val socket = serverSocket.accept()
+    val sendChannel = socket.openWriteChannel(autoFlush = true)
+    var i = 0
+    while (true) {
+        sendChannel.writeFully("Hello World! x = $i".toByteArray())
+        delay(1_000)
+        i++
+    }
+}
+
+suspend fun receiver() {
+    val selectorManager = SelectorManager(Dispatchers.IO)
+    val socket = aSocket(selectorManager).tcp().connect("127.0.0.1", 9002)
+
+    val receiveChannel = socket.openReadChannel()
+
+    while (true) {
+        receiveChannel.read {
+            val arr = it.moveToByteArray()
+            println(String(arr))
+        }
+    }
 }
